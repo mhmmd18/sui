447b4782d8f9fa0dabc120e257f224da08abd9ab
mhmmd18 <117086746+mhmmd18@users.noreply.github.com>
2023-09-09 23:51:49 +0700

    Create Modul : Splash, Intro & Login

---
+++ lib/core.dart
@@ -0,0 +1,11 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:sui/module/intro/controller/intro_controller.dart';
+export 'package:sui/module/intro/view/intro_view.dart';
+export 'package:sui/module/login/controller/login_controller.dart';
+export 'package:sui/module/login/view/login_view.dart';
+export 'package:sui/module/splash/controller/splash_controller.dart';
+export 'package:sui/module/splash/view/splash_view.dart';
