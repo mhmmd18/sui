b978e8cdca11ee000551902df764e522dc2a240c
mhmmd18 <117086746+mhmmd18@users.noreply.github.com>
2023-09-17 22:33:36 +0700

    Button Navigation Bottom with Stack

--- lib/module/register/view/register_view.dart
+++ lib/module/register/view/register_view.dart
@@ -14,108 +14,117 @@ class RegisterView extends StatefulWidget {
         leading: Container(),
         leadingWidth: 0,
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20.0,
-              ),
-              Center(
-                child: Image.asset(
-                  "assets/icon/icon.png",
-                  width: 100.0,
-                  height: 100.0,
-                  fit: BoxFit.fill,
-                ),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              QTextField(
-                label: "Full Name",
-                onChanged: (value) {},
-                onSubmitted: (value) {},
-              ),
-              const SizedBox(
-                height: 15.0,
-              ),
-              QTextField(
-                label: "Email",
-                onChanged: (value) {},
-                onSubmitted: (value) {},
-              ),
-              const SizedBox(
-                height: 15.0,
-              ),
-              QTextField(
-                label: "Password",
-                onChanged: (value) {},
-                onSubmitted: (value) {},
-                obscureText: true,
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              QButton(
-                label: "Create an Account",
-                onPressed: () {},
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                  text: '',
-                  style: const TextStyle(),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'By Signing up, you agree to our \n',
-                      style: TextStyle(color: disabledTextColor),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              controller: ScrollController(),
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20.0,
                     ),
-                    TextSpan(
-                      text: 'Terms',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: primaryColor,
+                    Center(
+                      child: Image.asset(
+                        "assets/icon/icon.png",
+                        width: 100.0,
+                        height: 100.0,
+                        fit: BoxFit.fill,
                       ),
                     ),
-                    TextSpan(
-                      text: ' and ',
-                      style: TextStyle(
-                        color: disabledTextColor,
-                      ),
+                    const SizedBox(
+                      height: 15.0,
                     ),
-                    TextSpan(
-                      text: 'Privacy',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: primaryColor,
+                    QTextField(
+                      label: "Full Name",
+                      onChanged: (value) {},
+                      onSubmitted: (value) {},
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    QTextField(
+                      label: "Email",
+                      onChanged: (value) {},
+                      onSubmitted: (value) {},
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    QTextField(
+                      label: "Password",
+                      onChanged: (value) {},
+                      onSubmitted: (value) {},
+                      obscureText: true,
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    QButton(
+                      label: "Create an Account",
+                      onPressed: () {},
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        text: '',
+                        style: const TextStyle(),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'By Signing up, you agree to our \n',
+                            style: TextStyle(color: disabledTextColor),
+                          ),
+                          TextSpan(
+                            text: 'Terms',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: primaryColor,
+                            ),
+                          ),
+                          TextSpan(
+                            text: ' and ',
+                            style: TextStyle(
+                              color: disabledTextColor,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'Privacy',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: primaryColor,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: Container(
-        width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            InkWell(
-              onTap: () => Navigator.pop(context),
-              child: Text(
-                "I have an account",
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                  color: primaryColor,
+            ),
+            Positioned(
+              left: 20,
+              right: 20,
+              bottom: 20,
+              child: InkWell(
+                onTap: () => Navigator.pop(context),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "I have an account",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                        color: primaryColor,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
